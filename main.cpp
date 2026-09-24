3bc86323d49c27b14f5d43120170ea0da77fa023
Gosha <goshashalnev@bk.ru>
2026-09-24 19:03:13 +0300

    Initial commit: Добавлена проверка ввода данных

--- main.cpp
+++ main.cpp
@@ -16,39 +16,141 @@ int main()
     double income = 0;
     double expense = 0;
     double balance = 0;
-
-    cout << "Система управления персональными финансами" << endl;
-
+    
     cout << "Введите категорию дохода: ";
     cin >> incomeCategory;
+
+    while (incomeCategory.empty())
+    {
+        cout << "Ошибка: поле не может быть пустым. Введите категорию дохода: ";
+        cin >> incomeCategory;
+    }
 
     cout << "Введите сумму дохода: ";
     cin >> income;
 
+    while (cin.fail())
+    {
+        cin.clear();
+        cin.ignore(10000, '\n');
+
+        cout << "Некорректный ввод. Введите число: ";
+        cin >> income;
+    }
+
+    while (income < 0)
+    {
+        cout << "Ошибка: сумма дохода не может быть отрицательной. Введите число: ";
+        cin >> income;
+
+        while (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(10000, '\n');
+
+            cout << "Некорректный ввод. Введите число: ";
+            cin >> income;
+        }
+    }
+
     cout << "Введите дату дохода: ";
     cin >> incomeDate;
+
+    while (incomeDate.empty())
+    {
+        cout << "Ошибка: поле не может быть пустым. Введите дату дохода: ";
+        cin >> incomeDate;
+    }
 
     cout << "Введите источник дохода: ";
     cin >> incomeSource;
 
+    while (incomeSource.empty())
+    {
+        cout << "Ошибка: поле не может быть пустым. Введите источник дохода: ";
+        cin >> incomeSource;
+    }
+
     cout << "Введите категорию расхода: ";
     cin >> expenseCategory;
+
+    while (expenseCategory.empty())
+    {
+        cout << "Ошибка: поле не может быть пустым. Введите категорию расхода: ";
+        cin >> expenseCategory;
+    }
 
     cout << "Введите сумму расхода: ";
     cin >> expense;
 
+    while (cin.fail())
+    {
+        cin.clear();
+        cin.ignore(10000, '\n');
+
+        cout << "Некорректный ввод. Введите число: ";
+        cin >> expense;
+    }
+
+    while (expense < 0)
+    {
+        cout << "Ошибка: сумма расхода не может быть отрицательной. Введите число: ";
+        cin >> expense;
+
+        while (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(10000, '\n');
+
+            cout << "Некорректный ввод. Введите число: ";
+            cin >> expense;
+        }
+    }
+
     cout << "Введите дату расхода: ";
     cin >> expenseDate;
+
+    while (expenseDate.empty())
+    {
+        cout << "Ошибка: поле не может быть пустым. Введите дату расхода: ";
+        cin >> expenseDate;
+    }
 
     cout << "Введите описание расхода: ";
     cin >> expenseDescription;
 
-    balance = income - expense;
+    while (expenseDescription.empty())
+    {
+        cout << "Ошибка: поле не может быть пустым. Введите описание расхода: ";
+        cin >> expenseDescription;
+    }
 
-    cout << endl;
-    cout << "Доход: " << income << endl;
-    cout << "Расход: " << expense << endl;
-    cout << "Текущий баланс: " << balance << endl;
+    if (income >= 0 && expense >= 0)
+    {
+        balance = income - expense;
+
+        cout << endl;
+        cout << "Доход: " << income << endl;
+        cout << "Расход: " << expense << endl;
+        cout << "Текущий баланс: " << balance << endl;
+
+        if (balance > 0)
+        {
+            cout << "Финансовый результат: доход превышает расходы." << endl;
+        }
+        else if (balance < 0)
+        {
+            cout << "Финансовый результат: расходы превышают доход." << endl;
+        }
+        else
+        {
+            cout << "Финансовый результат: доходы равны расходам." << endl;
+        }
+    }
+    else
+    {
+        cout << "Ошибка: доход и расход не могут быть отрицательными." << endl;
+    }
 
     return 0;
 }
